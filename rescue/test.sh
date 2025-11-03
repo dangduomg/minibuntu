a2e7383d36486771329a058cd0f1939fa1e5d581
dangduomg <85876662+dangduomg@users.noreply.github.com>
2025-11-03 09:32:52 +0700

    update test.sh to generate the iso directly

--- rescue/test.sh
+++ rescue/test.sh
@@ -9,30 +9,6 @@ if [[ ! -d root ]]; then
     source ./buildroot.sh
 fi
 
-fallocate -l 2G root.img
-rootloop=$(losetup -fP --show root.img)
+source ./buildiso.sh
 
-mkfs.ext4 root.img
-mkdir imgroot
-mount "${rootloop}" imgroot
-
-rsync -aHAX --numeric-ids root/ imgroot/
-
-cd imgroot
-
-systemd-nspawn bash -c "
-    useradd -m -s /bin/bash -G sudo,users ubuntu
-    passwd -du ubuntu
-"
-
-cd ..
-umount imgroot
-rmdir imgroot
-
-qemu-system-x86_64 -enable-kvm -cpu host -m 2G \
-    -kernel root/boot/vmlinuz \
-    -initrd root/boot/initrd.img -append 'root=/dev/sda rw' \
-    -hda root.img
-
-rm root.img
-losetup -d "$rootloop"
+qemu-system-x86_64 -enable-kvm -m 2G -cdrom "$iso_name"
