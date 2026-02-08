5e8689a4b3fb23839dbc0cc08eb446528b8124be
dangduomg <jack98765.2007@gmail.com>
2026-02-08 14:12:47 +0700

    add a new tool: mate-time-admin

--- canon/build-packages.sh
+++ canon/build-packages.sh
@@ -3,3 +3,5 @@
 mkdir -p oem/after/pkgs/
 dpkg-deb -b --root-owner-group calamares-settings/ \
     oem/after/pkgs/calamares-settings-minibuntu-canon.deb
+dpkg-deb -b --root-owner-group mate-time-admin/ \
+    oem/after/pkgs/mate-time-admin.deb
