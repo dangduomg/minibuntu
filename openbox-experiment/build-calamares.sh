f76f697668c401aa0e93c6b14cd12ed835d9e157
dangduomg <jack98765.2007@gmail.com>
2026-02-22 22:04:10 +0700

    remove openbox-experiment phase
    tint2 panel doesn't work and its outdated compared to minibuntu-canon

--- openbox-experiment/build-calamares.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dpkg-deb -b --root-owner-group calamares-settings/ \
-    oem/after/pkgs/calamares-settings-minibuntu-canon.deb
--- openbox-experiment/buildiso.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-source ../common/buildiso.sh minibuntu-openbox-experiment .
